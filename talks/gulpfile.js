f8c44c8383004ed8947ac20dd5f65c5169385030
疯狂紫萧 <545570335@qq.com>
2017-03-27 21:45:32 +0800

    add

---
+++ talks/gulpfile.js
@@ -0,0 +1,78 @@
+﻿//导入工具包
+var gulp = require('gulp'),
+    less = require('gulp-less'),                    //less处理插件
+    autoprefixer = require('gulp-autoprefixer'),    //css自动补全
+    imagemin = require('gulp-imagemin'),            //图片压缩
+    cache = require('gulp-cache'),                  //缓存管理
+    fileinclude = require('gulp-file-include'),     //文件替换管理
+    uglify = require('gulp-uglify'),                //js压缩插件
+    cssmin = require('gulp-clean-css'),             //css压缩
+    rename = require("gulp-rename"),                //文件重命名
+    del = require('del'),                           //删除文件
+    jshint = require("gulp-jshint");                //js的语法检测
+
+
+var version="1.0.0";
+//帮助
+gulp.task("help", function () {
+   
+});
+
+function setJavascript(path) {
+    gulp.src(path)
+        .pipe(jshint())
+        .pipe(gulp.dest('www/js'))
+}
+function setCss(path) {
+    gulp.src(path)
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Android >= 4.0'], cascade: true, remove: true
+        }))
+        .pipe(gulp.dest('www/css'))
+        .pipe(cssmin())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest('www/css'));
+}
+function setHtml(path) {
+    gulp.src(path)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file',
+            context: {
+                version: version
+            }
+        }))
+        .pipe(gulp.dest('src'))
+        .pipe(connect.reload());
+}
+function setImage(path) {
+    gulp.src(path)
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('www/images'))
+        .pipe(connect.reload());
+}
+//清空文件夹
+gulp.task('clean', function () {
+    del('www/*');
+});
+//使用live reload
+gulp.task('connect', function () {
+    connect.server({
+        //host:'localhost',
+        port: 8888,
+        root: 'testDecomposing/src/main/dist/',
+        livereload: true
+    });
+});
+
+//监测文件改变
+gulp.task('watch', function () {
+    //监控less
+    gulp.watch('src/css/*.less', function (event) {
+        setCss(event.path);
+    });
+  
+});
+
+gulp.task('default', ['watch']); //定义默认任务
---
+++ talks/index.js
@@ -0,0 +1,43 @@
+﻿var express = require('express'),
+    io = require('socket.io');
+
+    var app = express();
+    app.use(express.static(__dirname)+"/www");
+    var server = app.listen(8888);
+    var ws = io.listen(server);
+
+    ws.on('connection', function(client){
+	    console.log('\033[96msomeone is connect\033[39m \n');
+	    client.on('join', function(msg){
+	        // 检查是否有重复
+	        if(checkNickname(msg)){
+	            client.emit('nickname', '昵称有重复!');
+	        }else{
+	            client.nickname = msg;
+	            ws.sockets.emit('announcement', '系统', msg + ' 加入了聊天室!');
+	        }
+	    });
+	    // 监听发送消息
+	    client.on('send.message', function(msg){
+	        client.broadcast.emit('send.message',client.nickname,  msg);
+	    });
+	    // 断开连接时，通知其它用户
+	    client.on('disconnect', function(){
+	        if(client.nickname){
+	            client.broadcast.emit('send.message','系统',  client.nickname + '离开聊天室!');
+	        }
+	    })
+
+	})
+
+// 检查昵称是否重复
+var checkNickname = function(name){
+    for(var k in ws.sockets.sockets){
+        if(ws.sockets.sockets.hasOwnProperty(k)){
+            if(ws.sockets.sockets[k] && ws.sockets.sockets[k].nickname == name){
+                return true;
+            }
+        }
+    }
+    return false;
+}
