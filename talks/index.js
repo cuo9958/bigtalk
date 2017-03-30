76dec651e006d9ab1a7c0c91166717562b89888c
guofangchao <guofangchao@laibatour.com>
2017-03-30 18:37:39 +0800

    add

--- talks/index.js
+++ talks/index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + "/www"));
 var server = app.listen(8888);
 var ws = io.listen(server);
 //用户列表
-var userlist = [];
+var userlist = {};
 var User = function (client) {
     this.name = null;
     this.client = client;
@@ -23,33 +23,33 @@ var User = function (client) {
 };
 
 ws.on('connection', function (client) {
-    client.user = new User(client);
     //加入并创建个人对象
-    client.on('join', function (nickname) {
-        client.user.name = nickname;
-        userlist.push({
-            id: client.user.id,
-            name: client.user.name
-        });
+    client.on('join', function (nickname, x, y) {
+        var user = {
+            id: client.id,
+            name: nickname,
+            x: x,
+            y: y
+        };
+        userlist[client.id] = user;
         client.emit('joined', client.id, userlist);
-        ws.sockets.emit('add', client.id, client.user.name);
+        client.broadcast.emit('add', user);
     });
     // 监听发送消息
     client.on('send.message', function (msg) {
         client.broadcast.emit('send.message', client.id, msg);
     });
     //监听移动
-    client.on('move', function (msg) {
-       
+    client.on('move', function (x, y) {
+        var user = userlist[client.id];
+        user.x = x;
+        user.y = y;
+        client.broadcast.emit("moved", user);
     });
     // 断开连接时，通知其它用户
     client.on('disconnect', function () {
-        for (var i = 0; i < userlist.length; i++) {
-            if (userlist[i].sessionId == client.sessionId) {
-                userlist.splice(i, 1);
-            }
-        }
-        client.broadcast.emit('leave', client.sessionId);
+        delete userlist[client.id];
+        client.broadcast.emit('leave', client.id);
     });
 })
 
